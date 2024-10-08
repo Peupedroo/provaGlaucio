e7dd0c8dbce54b270c3de6fe7230c3332ad5c12c
pedro <pedrosilvagouveia@gmail.com>
2024-10-08 15:48:53 -0300

    enviando quarta

---
+++ Quarta_questao.js
@@ -0,0 +1,10 @@
+const aluno = {
+    nome : "pedro",
+    notas : [10, 9 , 8]
+};
+
+for (let i = 0; i < aluno.notas.length; i++) {
+        var conta = aluno.notas[i] / aluno.notas.length;
+
+};
+console.log(conta);
