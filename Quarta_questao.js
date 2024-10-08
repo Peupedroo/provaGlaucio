e2c8e0e9f55191b08919cf615281ff1ce54042b5
pedro <pedrosilvagouveia@gmail.com>
2024-10-08 15:59:09 -0300

    enviandom

--- Quarta_questao.js
+++ Quarta_questao.js
@@ -1,10 +1,14 @@
+import {i} from "vite/dist/node/types.d-aGj9QkWt.js";
+
 const aluno = {
     nome : "pedro",
     notas : [10, 9 , 8]
 };
-
+let soma = 0
 for (let i = 0; i < aluno.notas.length; i++) {
-        var conta = aluno.notas[i] / aluno.notas.length;
+        soma  += aluno.notas[i] ;
 
-};
-console.log(conta);
+}
+
+const media = soma / aluno.notas.length;
+console.log(media);
